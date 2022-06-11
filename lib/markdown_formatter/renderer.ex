b3c7c9eef651a5371933aa042712ef0af6762929
Eric Saxby <sax@livinginthepast.org>
2022-06-11 09:52:58 -0700

    RenderState as struct

--- lib/markdown_formatter/renderer.ex
+++ lib/markdown_formatter/renderer.ex
@@ -2,11 +2,20 @@ defmodule MarkdownFormatter.Renderer do
   # @related [test](/test/markdown_formatter/renderer_test.exs)
   @moduledoc false
 
+  defmodule RenderState do
+    @moduledoc false
+
+    defstruct ~w{parent}a
+
+    def new(), do: __struct__()
+    def parent(state, parent), do: %{state | parent: parent}
+  end
+
   @doc "Given AST produced from Earmark, turn it back into Markdown."
   @spec to_markdown(Earmark.ast()) :: binary()
   def to_markdown(ast) when is_list(ast) do
     ast
-    |> render([], nil)
+    |> render([], RenderState.new())
     |> to_string()
   end
 
@@ -40,10 +49,10 @@ defmodule MarkdownFormatter.Renderer do
   defp render({"strong", [], contents, %{}}, doc, opts), do: ["**", render(contents, [], opts), "**" | doc]
 
   # lists
-  defp render({"ol", [], contents, %{}}, doc, _opts), do: render(contents, doc, :ol)
-  defp render({"ul", [], contents, %{}}, doc, _opts), do: render(contents, doc, :ul)
-  defp render({"li", [], contents, %{}}, doc, :ol), do: doc ++ [render(contents, ["1. "], nil), "\n"]
-  defp render({"li", [], contents, %{}}, doc, :ul), do: doc ++ [render(contents, ["- "], nil), "\n"]
+  defp render({"ol", [], contents, %{}}, doc, opts), do: render(contents, doc, RenderState.parent(opts, :ol))
+  defp render({"ul", [], contents, %{}}, doc, opts), do: render(contents, doc, RenderState.parent(opts, :ul))
+  defp render({"li", [], contents, %{}}, doc, %{parent: :ol}), do: doc ++ [render(contents, ["1. "], nil), "\n"]
+  defp render({"li", [], contents, %{}}, doc, %{parent: :ul}), do: doc ++ [render(contents, ["- "], nil), "\n"]
 
   # text node
   defp render(text, doc, _opts) when is_binary(text), do: [text | doc]
