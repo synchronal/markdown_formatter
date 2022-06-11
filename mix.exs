5dc8b6a16fef1b313973b7dbb4df42e3e58c6bb2
Eric Saxby <sax@livinginthepast.org>
2022-06-10 23:57:20 -0700

    Add medic

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule MarkdownFormatter.MixProject do
     [
       app: :markdown_formatter,
       deps: deps(),
+      dialyzer: dialyzer(),
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
+      preferred_cli_env: [credo: :test, dialyzer: :test],
       start_permanent: Mix.env() == :prod,
       version: "0.1.0"
     ]
@@ -20,7 +22,19 @@ defmodule MarkdownFormatter.MixProject do
 
   defp deps do
     [
-      {:earmark, "~> 1.4"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:earmark, "~> 1.4"},
+      {:ex_doc, "~> 0.28.4", only: :dev, runtime: false},
+      {:mix_audit, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  def dialyzer do
+    [
+      plt_add_apps: [:ex_unit],
+      plt_add_deps: :app_tree,
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 
--- test/support/case.ex
+++ test/support/case.ex
@@ -1,4 +1,5 @@
 defmodule Test.Case do
+  @moduledoc false
   use ExUnit.CaseTemplate
 
   using do
