1dca1a035c863bd533afdfe6d6c0854a662b9979
Eric Saxby <sax@livinginthepast.org>
2022-06-12 14:35:23 -0700

    Add formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,5 @@
 [
+  plugins: [MarkdownFormatter],
   line_length: 120,
   inputs: ["{mix,.formatter}.exs", "{.medic,config,lib,test}/**/*.{ex,exs}"]
 ]
--- lib/markdown_formatter.ex
+++ lib/markdown_formatter.ex
@@ -2,5 +2,29 @@ defmodule MarkdownFormatter do
   @moduledoc """
   A formatter that can be plugged in to `mix format` in order to format Markdown
   files and sigils.
+
+  ## Usage
+
+  Add the `MarkdownFormatter` to `.formatter.exs`.
+
+  ```elixir`
+  [
+    # Define the desired plugins
+    plugins: [MixMarkdownFormatter],
+    # Remember to update the inputs list to include the new extensions
+    inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}", "posts/*.{md,markdown}"]
+  ]
+  ````
   """
+
+  @behaviour Mix.Tasks.Format
+
+  def features(_opts) do
+    [sigils: [:M], extensions: [".md", ".markdown"]]
+  end
+
+  def format(contents, _opts) do
+    {:ok, markdown_ast, []} = EarmarkParser.as_ast(contents)
+    MarkdownFormatter.Renderer.to_markdown(markdown_ast)
+  end
 end
